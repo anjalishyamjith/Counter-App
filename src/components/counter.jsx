135b5f81b2d20027d3a338b60517053c8d51d228
Anjali Shyamjith <anjalishyamjith05@gmail.com>
2021-07-25 13:56:35 +0530

    Exercise - Decrement Button

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -16,20 +16,31 @@ class Counter extends Component {
   render() {
     console.log('Counter - Rendered');
     return (
-      <div>
-        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button
-          onClick={() => this.props.onIncrement(this.props.counter)}
-          className="btn btn-secondary btn-sm"
-        >
-          Increment
-        </button>
-        <button
-          onClick={() => this.props.onDelete(this.props.counter.id)}
-          className="btn btn-danger btn-sm m-2"
-        >
-          Delete
-        </button>
+      <div className="row">
+        <div className="col-1">
+          <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        </div>
+        <div className="col">
+          <button
+            onClick={() => this.props.onIncrement(this.props.counter)}
+            className="btn btn-secondary btn-sm"
+          >
+            +
+          </button>
+          <button
+            onClick={() => this.props.onDecrement(this.props.counter)}
+            className="btn btn-secondary btn-sm m-2"
+            disabled={this.disableEnable()}
+          >
+            -
+          </button>
+          <button
+            onClick={() => this.props.onDelete(this.props.counter.id)}
+            className="btn btn-danger btn-sm"
+          >
+            x
+          </button>
+        </div>
       </div>
     );
   }
@@ -43,6 +54,11 @@ class Counter extends Component {
   formatCount() {
     const { value } = this.props.counter;
     return value === 0 ? 'Zero' : value;
+  }
+
+  disableEnable() {
+    const { value } = this.props.counter;
+    return value === 0 ? true : false;
   }
 }
 
